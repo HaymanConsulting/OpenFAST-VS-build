3f24918e7b42198ac40c927affa615857c9ecc9a
Greg Hayman <gregory.hayman@nrel.gov>
2017-01-10 14:34:39 -0700

    Builds for x64 and win32 now work for both debug and release.  However we may want to make additional changes so that the outputs from the various project builds are all marked either x64 or win32 and debug as appropriate.

--- RunRegistry.bat
+++ RunRegistry.bat
@@ -23,7 +23,7 @@ IF not "%2"=="" SET Root_Loc=%2
 
 SET Local_Modules_Loc=%Root_Loc%\modules-local
 SET Ext_Modules_Loc=%Root_Loc%\modules-ext
-SET Registry=..\build\bin\Registry_win32.exe
+SET Registry=..\build\bin\Registry.exe
 SET Output_Loc=..\build\types-files
 SET FAST_Loc=%Local_Modules_Loc%\fast-library\src
 SET ED_Loc=%Local_Modules_Loc%\elastodyn\src
